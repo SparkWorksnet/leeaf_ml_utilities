19ce4e06a4ad69c50046e734359181347133a108
Dimitrios Amaxilatis <d.amaxilatis@gmail.com>
2023-08-30 18:52:02 +0300

    update models location in repo

--- classification/leeaf_bench_classification.py
+++ classification/leeaf_bench_classification.py
@@ -10,16 +10,16 @@ IMAGE_SIZE = [256, 256]
 test_dataset = tf.keras.utils.image_dataset_from_directory(directory=test_image_path, image_size=IMAGE_SIZE,
                                                            batch_size=1, shuffle=False)
 
-models_dir = "models"
+models_dir = "../bench_models"
 prediction_times = {}
 dataset_size = len(test_dataset)
 
 for model in os.listdir(models_dir):
-    print(model)
-    existing_model = tf.keras.models.load_model(model)
-    start = time.time()
-    y_pred = existing_model.predict(test_dataset)
-    time_diff = time.time() - start
-    prediction_times[model] = time_diff / dataset_size
-
+    if model.endswith('.h5'):
+        print(model)
+        existing_model = tf.keras.models.load_model(model)
+        start = time.time()
+        y_pred = existing_model.predict(test_dataset)
+        time_diff = time.time() - start
+        prediction_times[model] = time_diff / dataset_size
 print(prediction_times)
